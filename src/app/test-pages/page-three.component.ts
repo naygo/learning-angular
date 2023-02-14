29004750b0f6fe76ddb95a182927bec2f762aa2f
Nayla Gomes <nayla.cgs@gmail.com>
2023-02-14 09:49:48 -0300

    passing data through the templates

--- src/app/test-pages/page-three.component.ts
+++ src/app/test-pages/page-three.component.ts
@@ -1,15 +1,46 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
+import { Component, Input, TemplateRef } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Movie } from './interfaces';
 
 @Component({
   selector: 'app-page-three',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgTemplateOutlet],
   template: `
-    <p routerLink="page-three">page-three works!</p>
-  `
+    <p routerLink="page-three" style="color: red">page-three works!</p>
+
+    <!-- declarative way to create an embedded view, here we can dynamically pass templates -->
+    <ng-container
+      [ngTemplateOutlet]="template || defaultTemplate"
+      [ngTemplateOutletContext]="{ $implicit: data }"
+    >
+    </ng-container>
+    <ng-template #defaultTemplate>
+      <div style="background-color: bisque; width: 300px">
+        <p>Beautiful Title</p>
+        <small>cool text inside a embedded view</small>
+
+        <p>{{ data.title }}</p>
+        <p>{{ data.year }}</p>
+        <p>{{ data.rating }}</p>
+      </div>
+    </ng-template>
+  `,
 })
 export class PageThreeComponent {
+  @Input() template: TemplateRef<Movie>;
 
+  data: Movie = {
+    title: 'The Matrix',
+    year: 1999,
+    rating: 8.7,
+  };
+  // imperative way to create an embedded view
+  // @ViewChild('container', { read: ViewContainerRef }) container: ViewContainerRef;
+  // @ViewChild('defaultTemplate') defaultTemplate: TemplateRef<any>;
+
+  // ngAfterViewInit() {
+  //   this.container.createEmbeddedView(this.defaultTemplate);
+  // }
 }
--- src/app/test-pages/page-two.component.ts
+++ src/app/test-pages/page-two.component.ts
@@ -1,17 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PageThreeComponent } from './page-three.component';
 
 @Component({
   selector: 'app-page-two',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, PageThreeComponent],
   template: `
     <p routerLink="page-two">page-two works!</p>
-  `,
-  styles: [
-  ]
-})
-export class PageTwoComponent {
+    <app-page-three [template]="otherTemplate"></app-page-three>
 
-}
+    <ng-template #otherTemplate let-data>
+      <div style="background-color: cornflowerblue; width: 300px">
+        <p>Other Beautiful Title</p>
+        <small>other cool text redered with template outlet</small>
+
+        <p>{{ data.title }}</p>
+        <p>{{ data.year }}</p>
+        <p>{{ data.rating }}</p>
+      </div>
+    </ng-template>
+  `,
+  styles: [],
+})
+export class PageTwoComponent {}
